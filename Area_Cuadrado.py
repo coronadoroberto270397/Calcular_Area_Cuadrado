f8855b71544dd079812032f5e37f2c5621f20eb2
“Tu <robertocorondocoto@gmail.com>
2020-07-31 22:21:47 -0500

    Se empezo haciendo una funcion de suma

---
+++ Area_Cuadrado.py
@@ -0,0 +1,10 @@
+#Primer prueba de programacion 21/07/2020
+#Area_Cuadrado
+
+def suma(a,b):
+    igual=a+b
+    return igual
+
+a=int(input("Escribe un numero:",))
+b=int(input("Escribe un numero:",))
+print (suma(a,b))
